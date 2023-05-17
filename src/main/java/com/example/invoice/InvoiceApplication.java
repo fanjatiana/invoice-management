77990459c83722e028c2859a19047e8f7cf515e8
fanjatiana.pavageau <fanjatiana.pavageau@outlook.com>
2023-05-17 09:25:21 +0200

    add folders and configure jpa

--- src/main/java/com/example/invoice/InvoiceApplication.java
+++ src/main/java/com/example/invoice/InvoiceApplication.java
@@ -1,13 +1,18 @@
 package com.example.invoice;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class InvoiceApplication {
+public class InvoiceApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InvoiceApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
 }
