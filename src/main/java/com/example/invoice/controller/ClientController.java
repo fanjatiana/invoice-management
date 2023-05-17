b050a9ad3a105a0bdf5885fd2f185e11abbaabba
fanjatiana.pavageau <fanjatiana.pavageau@outlook.com>
2023-05-17 12:09:19 +0200

    feat:create add client form

--- src/main/java/com/example/invoice/controller/ClientController.java
+++ src/main/java/com/example/invoice/controller/ClientController.java
@@ -1,12 +1,17 @@
 package com.example.invoice.controller;
 
+import com.example.invoice.entity.Address;
 import com.example.invoice.entity.Client;
 import com.example.invoice.repository.ClientRepository;
 import com.example.invoice.service.ClientService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -20,5 +25,22 @@ public class ClientController {
         List<Client> clientList = clientService.findAllClients();
         model.addAttribute("clients", clientList);
         return "clients";
+    }
+
+    @GetMapping("/add-client")
+    public String displayClientForm() {
+        return "add-client";
+    }
+
+    @PostMapping("/add-client")
+    public String handleClientForm(@RequestParam String firstname,
+                                    @RequestParam String lastname,
+                                    @RequestParam String phoneNumber,
+                                    @RequestParam String address,
+                                    @RequestParam String city,
+                                    @RequestParam String zipCode,
+                                    @RequestParam String compagnyName) {
+        clientService.addClientByForm(firstname,lastname,phoneNumber,compagnyName,new Address(address,city,zipCode));
+        return "redirect:/clients";
     }
 }
--- src/main/java/com/example/invoice/service/ClientService.java
+++ src/main/java/com/example/invoice/service/ClientService.java
@@ -6,6 +6,7 @@ import com.example.invoice.entity.Role;
 import com.example.invoice.entity.User;
 import com.example.invoice.repository.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.datasource.AbstractDriverBasedDataSource;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -21,6 +22,12 @@ public class ClientService {
         Client client1 = clientRepository.save(new Client("Marc","Dupuit","0212254689","JoahVap",new Address("3 rue des marais","44600","Saint-Nazaire")));
         clients.add(client1);
         return clients;
+    }
+
+    public Client addClientByForm(String firstname, String lastname, String phoneNumber, String compagnyName, Address address){
+        Client newClient = new Client(firstname,lastname,phoneNumber,compagnyName,new Address(address.getStreetName(),address.getCity(),address.getZipCode()));
+        clientRepository.save(newClient);
+        return newClient;
     }
 
     public List<Client> findAllClients(){
